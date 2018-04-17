2b7820a91821ec18e812df35791288a7e3d355ff
matiTechno <mateusz.macieja8@gmail.com>
2018-04-17 19:59:39 +0200

    removed unused source file

--- stb.c
+++
@@ -1,6 +0,0 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#define STB_RECT_PACK_IMPLEMENTATION
-#include "imgui/stb_rect_pack.h"
-#define STB_TRUETYPE_IMPLEMENTATION
-#include "imgui/stb_truetype.h"
